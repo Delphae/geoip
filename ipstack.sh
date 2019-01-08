fe9886a9e8e4af47ed907430ff6e83104bfffbcd
delphae <45516472+Delphae@users.noreply.github.com>
2019-01-08 13:30:20 +0100

    Update ipstack.sh

--- ipstack.sh
+++ ipstack.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apikey="3fc7e044a32e37a199b33879210dfcda"
+apikey="3fc7e044a2e7a1993387910dfcda"
 #ipaddress=$(curl -s "http://ifconfig.co")
 #url="http://api.ipstack.com/$ipaddress?access_key=$apikey"
 url="http://api.ipstack.com/check?access_key=$apikey"
