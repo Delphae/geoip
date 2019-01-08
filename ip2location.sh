481a940d5cab2f825d9e9d1c4630a8b6535d8ccc
delphae <45516472+Delphae@users.noreply.github.com>
2019-01-08 15:36:09 +0100

    Add files via upload

--- ip2location.sh
+++ ip2location.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 ipaddress=$(curl -s "ifconfig.co")
-url="http://api.ip2location.com/?key=demo&package=WS12&format=json&ip=$ipaddress"
+package="WS12"
+#package="WS14"
+#package="WS8"
+url="http://api.ip2location.com/?key=demo&package=$package&format=json&ip=$ipaddress"
 curl -s $url | python -m json.tool > ip2location.json
 cat ip2location.json
 
