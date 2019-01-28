e2abca864d449e431c5e284b040c443e104a3d1e
delphae <45516472+Delphae@users.noreply.github.com>
2019-01-28 13:54:04 +0100

    Update dbip.sh

--- dbip.sh
+++ dbip.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 ipaddress=$(curl -s "ifconfig.co")
-url="http://api.ip2location.com/?key=demo&package=$package&format=json&ip=$ipaddress"
 url="http://api.db-ip.com/v2/free/$ipaddress"
 curl -s $url 
 
